a0d7687237518009b35b201d64ff037b6bd977a5
sujiajun <dennycai@qq.com>
2021-01-21 01:45:56 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+#
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-# 基本不需要添加啥插件了,我搜集了各位大神的插件都集成一个软件包直接打入源码里面了
-# 要了解增加了什么东西，就到我的专用软件包里面看看看吧，如果还是没有你需要的插件，请不要一下子就拉取别人的插件包
-# 相同的文件都拉一起，因为有一些可能还是其他大神修改过的容易造成编译错误的
-# 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
-# 软件包地址：https://github.com/281677160/openwrt-package
+# Uncomment a feed source
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add luci-theme-argon
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+rm -rf ../lean/luci-theme-argon
